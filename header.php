0eb9d10db5eecd320afacf24bd3221f91129cf35
a.zgurya <a.zgurya@mixis.net>
2015-03-12 10:30:21 +0200

    Add site-navigation

--- header.php
+++ header.php
@@ -7,12 +7,20 @@
 </head>
 <body <?php body_class(); ?>>
 	<div class="wrap">
+		<?php if ( has_nav_menu( 'top') ):?>
+			<nav class="top-menu">
+				<?php wp_nav_menu( array( 'theme_location'=>'top','container' => false,'items_wrap'=> '<ul>%3$s</ul>') ); ?>
+			</nav><!-- end top site-navigation -->
+		<?php endif;?>
 		<header class="site-header">
 			<div class="branding-header">
-				<div class="col-1">
-                	<a href="<?php echo get_site_url();?>"><img src="<?php bloginfo('template_url'); ?>/images/logo-main-4.1.png"></a>
-                </div>
+				<div class="col-1"></div>
                 <div class="col-2"></div>
-				<div class="col-3">
-                </div>
+				<div class="col-3"></div>
 			</div>
+		</header><!-- end site-header -->
+		<?php if ( has_nav_menu( 'main') ):?>
+			<nav class="main-menu">
+				<?php wp_nav_menu( array( 'theme_location'=>'main','container' => false,'items_wrap'=> '<ul>%3$s</ul>') ); ?>
+			</nav><!-- end main site-navigation -->
+		<?php endif;?>
